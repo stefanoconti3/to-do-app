8dc832640dd61cbbef90cfbff7cc459a6e49195c
Stefano Conti <stefano.conti33@outlook.com>
2023-08-23 11:55:06 +0200

    update task modal component

--- src/components/CreateTaskModal.vue
+++ src/components/CreateTaskModal.vue
@@ -1,34 +1,46 @@
 <template>
   <b-modal
-    ref="modal-create-task"
-    hide-footer
-    hide-header
-    size="lg"
-    modal-class="modal-create-task"
-    centered
-    v-model="showModal"
+      ref="modal-create-task"
+      hide-footer
+      hide-header
+      size="lg"
+      modal-class="modal-create-task"
+      centered
+      v-model="showModal"
   >
-      <h6 class="text-left font-weight-bold">
-        Create Task
-      </h6>
+    <h3 class="text-left font-weight-bold">
+      Create Task
+    </h3>
+    <div>
+      <p class="mb-0">Task Name:</p>
       <b-form-input
         name="task"
         required
         class="p-3"
         v-model="model.task"
       />
-      <b-button
+    </div>
+    <b-form-checkbox
+      id="checkbox-1"
+      v-model="model.mark"
+      name="checkbox-1"
+      :value="true"
+      :unchecked-value="false"
+    >
+      I mark task as complete
+    </b-form-checkbox>
+    <b-button
         variant="primary"
         @click="createTask"
-      >
-        Create
-      </b-button>
-      <b-button
+    >
+      Create
+    </b-button>
+    <b-button
         variant="danger"
         @click="() => {this.showModal = false}"
-      >
-        Cancel
-      </b-button>
+    >
+      Cancel
+    </b-button>
   </b-modal>
 </template>
 
